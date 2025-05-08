8f75d4162484e24b7fd9733bbeeb5df91299c860
Isaac Anderson <redranger.anderson@gmail.com>
2025-05-07 20:16:40 -0500

    Download Update
    Should be functional now "fingers crossed".

--- src/Download.jsx
+++ src/Download.jsx
@@ -7,14 +7,23 @@ function Download () {
           3. download csv file
         */
         let storage = localStorage.getItem("List")
-        let invObjArr = JSON.parse(storage)
+        let list = JSON.parse(storage)
         let inventory = []
         // console.log(inventory[2].item)
-        for (let i = 0; i < invObjArr.length; i += 1) {
-            inventory.push(invObjArr[i].item)
-            inventory.push(invObjArr[i].count)
-            console.log(inventory)
+        for (let i = 0; i < list.length; i += 1) {
+            let row = [];
+            row.push(list[i].item)
+            row.push(list[i].count)
+            inventory.push(row)
         }
+        const content = "data:text/csv;charset=utf-8," + inventory.map(row => row.join(",")).join("\n")
+        const uri = encodeURI(content)
+        const link = document.createElement("a")
+        link.setAttribute("href", uri);
+        link.setAttribute("download", "test");
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
     }
 
     return (
